07b98aca2d3f3f1bce8f52c5637e54741cc23701
Nickita <vasilev.nickita@yandex.ru>
2024-10-14 16:03:10 +0300

    UPDATE: mod data structure

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from typing import Any
-from fastapi import FastAPI, Depends, HTTPException, Form, Request
+import asyncio
+from typing import Any, Annotated
+from fastapi import FastAPI, Depends, HTTPException, Form, Request, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from models import Message
-
 
 achat = FastAPI(
     title="AChat",
@@ -13,17 +13,51 @@ achat.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory='templates')
 
+"""
+Главная страница чата
+"""
 
-@achat.get("/chat", response_class=HTMLResponse)
+
+@achat.get("/chat", response_class=HTMLResponse, status_code=200)
 async def get_started(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
 
-@achat.post("/sm")
+"""
+Метод отправки сообщений
+"""
+
+
+@achat.post("/sm", status_code=200)
 async def send_message(request: Request, message: Message = Form(...)):
-    return templates.TemplateResponse("chat.html", {"request": request})
+    return templates.TemplateResponse("chat.html", {"request": request, "message": message})
 
 
-@achat.get("/", response_class=HTMLResponse)
+"""
+Приветственная страница чата (регистрация/вход)
+"""
+
+
+@achat.get("/", response_class=HTMLResponse, status_code=200)
 async def get_started(request: Request):
     return templates.TemplateResponse("welcome.html", {"request": request})
+
+
+"""
+Методы загрузки файлов в чат
+"""
+
+
+@achat.post("/files")
+async def create_file(file: Annotated[bytes, File()]):
+    await asyncio.sleep(1)
+    if len(file) > 41943040:
+        raise HTTPException(status_code=400, detail="Большой размер файла")
+    return {"file_size": len(file)}
+
+
+@achat.post("/uploadfile")
+async def create_upload_file(file: UploadFile):
+    if len(file.filename) > 41943040:
+        raise HTTPException(status_code=400, detail="Большой размер файла")
+    return {"filename": file.filename}
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Column, Integer, String, Boolean
+# from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-from pydantic import BaseModel, validator
-
+from pydantic import BaseModel, StringConstraints
+from typing_extensions import Annotated
 
 Base = declarative_base()
 
 
 class Message(BaseModel):  # Определяем модель структуры данных для /sm, а так же валидируем данные
-    message: str
+    message: Annotated[str, StringConstraints(max_length=140)]
 
-    @validator('message')
-    def message_length(cls, v):
-        if len(v) > 140:
-            raise ValueError("Введите не более 140 символов")
-        return v
+    # @validator('message')
+    # def message_length(cls, v):
+    #     if len(v) > 140:
+    #         raise ValueError("Введите не более 140 символов")
+    #     return v
